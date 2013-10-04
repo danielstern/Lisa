b41a524ce5ae289c7825548223f249daa8ccbd03
daniel stern <daniel@danielstern.ca>
2013-10-04 11:54:13 -0400

    getting require on in there

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-main.js
+// Filename: main.js
+
+// Require.js allows us to configure shortcut alias
+// There usage will become more apparent further along in the tutorial.
+require.config({
+  paths: {
+    jquery: 'libs/jquery/jquery-2.0.3.min',
+    underscore: 'libs/underscore/lodash.underscore-min',
+    underscore_string: 'libs/underscore/underscore.string.min',
+    angular: 'libs/angular/min',
+    bootstrap: 'libs/bootstrap/bootstrap.min'
+  }
+
+});
+
